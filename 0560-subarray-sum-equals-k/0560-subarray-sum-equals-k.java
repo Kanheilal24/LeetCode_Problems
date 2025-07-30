7609ddd5c9923f787d57d7df9a0fd84ddc17b33c
Kanheilal Mahapatra <139961989+Kanheilal24@users.noreply.github.com>
2025-07-30 10:55:45 +0530

    Time: 1083 ms (23.97%), Space: 46.7 MB (33.74%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,3 +1,19 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int n = nums.length;
+//         int sum = 0, count = 0, start = 0;
+//         for(int end = 0; end < n; end++) { 
+//             sum += nums[end];
+//             while(sum > k && start <= end) {
+//                 sum -= nums[start++];
+//             }
+//             if(sum == k) count++;
+//         }
+//         return count;
+//     }
+// }
+
+
 class Solution {
     public int subarraySum(int[] arr, int k) {
         int n = arr.length;
@@ -16,3 +32,9 @@ class Solution {
         return count;
     }
 }
+
+//Yahan par hum sliding window use kar rahein hain.
+//Uspar maine ek start and end position liya hai and dono ka postition 0 se start kiya ,
+//phir ek sum var main end position add karliya , phir check kiya ki sum target se bada hai ki nahi and start position end postion se chota or uske barabar hai ki nahi.
+//agar ye dono condition true horahehein then maine sum se starting postion ka value minus karliya and start position ko ++ karliya.
+//phir check kiya ki sum == target ke bara bar hai ki nahi ,agar hai toh count++ karliya and count ko return karliya.
